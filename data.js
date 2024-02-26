430a64f23ee2f634f39fdf879f156b321b95fe87
sanjayy10 <sanjaya.ksks18@gmail.com>
2024-02-26 16:18:18 +0530

    simple

---
+++ data.js
@@ -0,0 +1,17 @@
+let x = 3
+let y = 5
+
+console.log(x+y);
+console.log(x-y);
+console.log(x/y);
+console.log(x*y);
+console.log(x%y);
+
+let a=5
+a++;
+console.log(a);
+
+
+let b=4
+b--;
+console.log(b);
---
+++ variable.js
@@ -0,0 +1,8 @@
+let name ="sanjay"
+let age = 21
+let accountno = 3454678394743
+let isactive = true
+cosole.console.log(name);
+console.log(age);
+console.log(accountno);
+console.log(isactive) ;
